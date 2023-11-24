bc1753bb4c8bdaa55a1fcec973b45355a68a5959
harim <harim@debian>
2023-11-24 09:10:52 -0500

    basic node health checkup script

---
+++ nodeHealth.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+###################
+# Author: Harim Woo
+# Date: 11/24/23
+# This script outputs the node health 
+# Version: v1
+###################
+
+set -x # Debug Mode
+
+df -h 
+
+free -g 
+
+nproc
+
+
