c3c1e80ef527863ce74e84964e582707fb44f8ce
harim <harim@debian>
2023-11-24 09:51:58 -0500

    add cur process command

--- nodeHealth.sh
+++ nodeHealth.sh
@@ -7,12 +7,18 @@
 # Version: v1
 ###################
 
+# set -xeo pipefail
 set -x # Debug Mode
+set -e # exit script if error
+set -o pipefail # catch pipe error 
 
-df -h 
-
-free -g 
-
+# disc space useage
+df -h
+# memory usage
+free -g
+# n of processes
 nproc
-
-
+# current processes 
+# use grep or awk to manipulate output
+# ex) ps -ef | grep harimwoo | awk -F" " '{print $2}'
+ps -ef
